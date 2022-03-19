733fb739655df902756d9c6b461075b7871bd9cb
AntonG-TAU <73164858+AntonG-TAU@users.noreply.github.com>
2022-03-19 23:40:15 +0200

    Update car_core.py

--- multiAgentEnv/maenv/mpe/_mpe_utils/car_core.py
+++ multiAgentEnv/maenv/mpe/_mpe_utils/car_core.py
@@ -138,32 +138,6 @@ class World:  # multi-agent world
         model_pred = self.forward_model.predict_batch(actions)
         return model_pred
 
-    # gather agent action forces
-    # def apply_action_force(self, p_force):
-    #     # set applied forces
-    #     for i, agent in enumerate(self.agents):
-    #         if agent.movable:
-    #             noise = np.random.randn(*agent.action.u.shape) * agent.u_noise if agent.u_noise else 0.0
-    #             p_force[i] = agent.action.u + noise
-    #     return p_force
-
-    # gather physical forces acting on entities
-    # def apply_environment_force(self, p_force):
-    #     # simple (but inefficient) collision response
-    #     for a, entity_a in enumerate(self.entities):
-    #         for b, entity_b in enumerate(self.entities):
-    #             if(b <= a):
-    #                 continue
-    #             [f_a, f_b] = self.get_collision_force(entity_a, entity_b)
-    #             if(f_a is not None):
-    #                 if(p_force[a] is None):
-    #                     p_force[a] = 0.0
-    #                 p_force[a] = f_a + p_force[a]
-    #             if(f_b is not None):
-    #                 if(p_force[b] is None):
-    #                     p_force[b] = 0.0
-    #                 p_force[b] = f_b + p_force[b]
-    #     return p_force
 
     def integrate_state(self, pose_diff):
         for i, agent in enumerate(self.agents):
@@ -171,20 +145,7 @@ class World:  # multi-agent world
             agent.state.p_pos += rel_pos_diff
             agent.state.orient += pose_diff[i,2]
             agent.state.orient = clip_angle(agent.state.orient)
-
-    # integrate physical state
-    # def integrate_state(self, p_force):
-    #     for i, entity in enumerate(self.entities):
-    #         if not entity.movable:
-    #             continue
-    #         entity.state.p_vel = entity.state.p_vel * (1 - self.damping)
-    #         if (p_force[i] is not None):
-    #             entity.state.p_vel += (p_force[i] / entity.mass) * self.dt
-    #         if entity.max_speed is not None:
-    #             speed = np.sqrt(np.square(entity.state.p_vel[0]) + np.square(entity.state.p_vel[1]))
-    #             if speed > entity.max_speed:
-    #                 entity.state.p_vel = entity.state.p_vel / np.sqrt(np.square(entity.state.p_vel[0]) + np.square(entity.state.p_vel[1])) * entity.max_speed
-    #         entity.state.p_pos += entity.state.p_vel * self.dt
+            
 
     def update_agent_state(self, agent):
         # set communication state (directly for now)
@@ -194,21 +155,3 @@ class World:  # multi-agent world
             noise = np.random.randn(*agent.action.c.shape) * agent.c_noise if agent.c_noise else 0.0
             agent.state.c = agent.action.c + noise
 
-    # get collision forces for any contact between two entities
-    # def get_collision_force(self, entity_a, entity_b):
-    #     if (not entity_a.collide) or (not entity_b.collide):
-    #         return [None, None]  # not a collider
-    #     if (entity_a is entity_b):
-    #         return [None, None]  # don't collide against itself
-    #     # compute actual distance between entities
-    #     delta_pos = entity_a.state.p_pos - entity_b.state.p_pos
-    #     dist = np.sqrt(np.sum(np.square(delta_pos)))
-    #     # minimum allowable distance
-    #     dist_min = entity_a.size + entity_b.size
-    #     # softmax penetration
-    #     k = self.contact_margin
-    #     penetration = np.logaddexp(0, -(dist - dist_min) / k) * k
-    #     force = self.contact_force * delta_pos / dist * penetration
-    #     force_a = +force if entity_a.movable else None
-    #     force_b = -force if entity_b.movable else None
-    #     return [force_a, force_b]
