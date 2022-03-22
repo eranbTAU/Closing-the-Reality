59f83ba3a9900a12959f78215d6a53ce9706a50b
AntonG-TAU <73164858+AntonG-TAU@users.noreply.github.com>
2022-03-22 11:33:37 +0200

    Update run_car.sh

--- multiAgentEnv/MARL/DDPG/run_car.sh
+++ multiAgentEnv/MARL/DDPG/run_car.sh
@@ -4,6 +4,7 @@ PYTHONPATH=/home/roblab1/PycharmProjects/MultiAgentEnv python main.py \
 --experiment_name test \
 --robot_type car \
 --algorithm ddpg \
+--is_train
 --max_steps 150 \
 --episodes 50 \
 --num_agents 5 \
